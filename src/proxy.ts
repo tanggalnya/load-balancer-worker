4e3e3b24da6ce59b4458d3765200b7bf5b8808d9
I Dewa Putu Ardi Nusawan <ardi.nusawan13@gmail.com>
2024-02-03 00:47:47 +0700

    cache healthcheck for 5 minutes

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,3 +1,6 @@
+const healthCheckCache = new Map<string, boolean>();
+const healthCheckCacheTTL = 300000; // Cache TTL in milliseconds (5 minutes)
+
 export default {
 	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
 		const url = new URL(request.url);
@@ -11,22 +14,46 @@ export default {
 		];
 
 		let index = Math.floor(Math.random() * backendServers.length);
-
 		let proxyUrl = backendServers[index];
+
+		// Check if health status is cached
+		const cachedHealthStatus = healthCheckCache.get(proxyUrl);
+		if (cachedHealthStatus !== undefined && cachedHealthStatus) {
+			let res = await fetch(proxyUrl + url.pathname + url.search, request);
+			return res;
+		}
+
 		let healthCheckResponse = await fetch(proxyUrl + '/ping'); // Perform health check
 
 		if (healthCheckResponse.ok) {
-			let res = await fetch(proxyUrl + url.pathname + url.search, request);
+			healthCheckCache.set(proxyUrl, true);
+			setTimeout(() => {
+				healthCheckCache.delete(proxyUrl); // Remove cache after TTL
+			}, healthCheckCacheTTL);
 
+			let res = await fetch(proxyUrl + url.pathname + url.search, request);
 			return res;
 		}
 
 		for (let i = 1; i < backendServers.length; i++) {
 			index = (index + 1) % backendServers.length;
 			proxyUrl = backendServers[index];
+
+			// Check if health status is cached
+			const cachedHealthStatus = healthCheckCache.get(proxyUrl);
+			if (cachedHealthStatus !== undefined && cachedHealthStatus) {
+				let res = await fetch(proxyUrl + url.pathname + url.search, request);
+				return res;
+			}
+
 			healthCheckResponse = await fetch(proxyUrl + '/ping');
 
 			if (healthCheckResponse.ok) {
+				healthCheckCache.set(proxyUrl, true);
+				setTimeout(() => {
+					healthCheckCache.delete(proxyUrl); // Remove cache after TTL
+				}, healthCheckCacheTTL);
+
 				let res = await fetch(proxyUrl + url.pathname + url.search, request);
 				return res;
 			}
